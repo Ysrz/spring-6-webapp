feb8c60c4d6a84d5bd305874a7ded6a631652d1d
yvonne-zwiep <yvonnezwiep@hotmail.com>
2025-04-17 14:54:35 +0200

    Create service layer

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookService.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Book;
+
+/**
+ * Created by Yvonne.
+ **/
+public interface BookService {
+
+    Iterable<Book> findAll();
+}
