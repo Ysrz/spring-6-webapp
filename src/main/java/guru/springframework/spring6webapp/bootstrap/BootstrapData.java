a337f674bd8027f6875106eeedcc03774a72eb5c
yvonne-zwiep <yvonnezwiep@hotmail.com>
2025-04-17 13:37:38 +0200

    Create new publisher in BootstrapData class

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,12 +2,12 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.sql.SQLOutput;
 
 /**
  * Created by Yvonne.
@@ -17,10 +17,14 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -55,6 +59,16 @@ public class BootstrapData implements CommandLineRunner {
         
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
-        System.out.println("Book Count:" + bookRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("Publisher Name");
+        publisher.setAddress("Address");
+        publisher.setCity("City");
+        publisher.setState("State");
+        publisher.setZip("Zip");
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
